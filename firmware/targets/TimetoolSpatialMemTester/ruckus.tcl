10c202e1c1e3ba08d389671da76a970e27786dd1
Larry Ruckman <ruckman@slac.stanford.edu>
2019-06-12 21:20:38 -0700

    ddr bug fix

--- firmware/targets/TimetoolSpatialMemTester/ruckus.tcl
+++ firmware/targets/TimetoolSpatialMemTester/ruckus.tcl
@@ -3,8 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load base sub-modules
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/axi-pcie-core/hardware/XilinxKcu1500
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/axi-pcie-core/hardware/XilinxKcu1500/ddr
+loadRuckusTcl $::env(PROJ_DIR)/../../submodules/axi-pcie-core/hardware/XilinxKcu1500
 loadRuckusTcl $::env(PROJ_DIR)/../../common
 
 # Load local source Code and constraints
